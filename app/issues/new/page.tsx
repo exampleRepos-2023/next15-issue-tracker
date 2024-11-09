6c420a001407348481c085fe2f25702aaff43858
NecipAkgz <nexco@hotmail.com>
2024-11-09 03:04:51 +0300

    feat(issue-form): improve form validation and error handling
    Enhanced input validation by adding custom error messages for title and description fields, improving user feedback. Added error handling in the form submission process to inform users of submission issues.

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,8 @@
 "use client";
-import { Button, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 
 interface IssueForm {
@@ -12,25 +13,33 @@ interface IssueForm {
 export default function NewIssuePage() {
   const router = useRouter();
   const { register, handleSubmit } = useForm<IssueForm>();
+  const [error, setError] = useState("");
 
   return (
-    <form
-      className="max-w-xl space-y-3"
-      onSubmit={handleSubmit(async (data) => {
-        axios.post("/api/issues", data);
-
-        router.push("/issues");
-      })}
-    >
-      <TextField.Root
-        placeholder="Title"
-        {...register("title")}
-      ></TextField.Root>
-      <TextArea
-        placeholder="Description"
-        {...register("description")}
-      ></TextArea>
-      <Button>Submit New Issue</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" size="1" className="mb-4">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form
+        className="max-w-xl space-y-3"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post("/api/issues", data);
+            router.push("/issues");
+          } catch (error) {
+            setError("Something went wrong");
+          }
+        })}
+      >
+        <TextField.Root
+          placeholder="Title"
+          {...register("title")}
+        ></TextField.Root>
+        <TextArea placeholder="Description" {...register("description")} />
+        <Button>Submit New Issue</Button>
+      </form>
+    </div>
   );
 }
