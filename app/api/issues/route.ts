90a4f782e4bbe515a644a5bee77401f1aec1de88
NecipAkgz <nexco@hotmail.com>
2024-11-09 03:55:22 +0300

    feat(validation): implement centralized issue creation validation schema
    Adopt a new validation schema for issue creation, enhancing error handling in the form and centralizing validation logic. This improves maintainability and scalability of the validation process across the application.

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,15 +1,10 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
-import z from "zod";
-
-const schema = z.object({
-  title: z.string().min(1, "Title is required").max(255),
-  description: z.string().min(1, "Description is required"),
-});
+import { createIssueSchema } from "../../validationSchemas";
 
 export const POST = async (request: NextRequest) => {
   const body = await request.json();
-  const validation = schema.safeParse(body);
+  const validation = createIssueSchema.safeParse(body);
 
   if (!validation.success)
     return NextResponse.json(
