96ef61e5e747b7b090cd6e5aafe68290191fe426
NecipAkgz <nexco@hotmail.com>
2024-11-11 03:15:14 +0300

    feat(issue-details): enhance issue detail layout with an edit button
    Improved the issue detail page UI by adding an edit button, enabling easier access for users to modify issues. Adjusted layout to a grid format for better organization and readability.

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { IssueStatusBadge } from "@/app/components";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 import ReactMarkdown from "react-markdown";
 
@@ -11,26 +13,35 @@ interface Props {
 }
 
 export default async function IssueDetailPage({ params }: Props) {
+  const { id } = await params;
   const issue = await prisma.issue.findUnique({
     where: {
-      id: Number(params.id),
+      id: Number(id),
     },
   });
 
   if (!issue) notFound();
 
   return (
-    <div>
-      <Heading>{issue.title}</Heading>
-      <Flex gap="3" align="center" my={"2"}>
-        <IssueStatusBadge status={issue.status} />
-        <Text weight={"bold"} size="2">
-          {issue.createdAt.toDateString()}
-        </Text>
-      </Flex>
-      <Card className="prose mt-4 dark:prose-invert">
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap={"4"}>
+      <Box>
+        <Heading>{issue.title}</Heading>
+        <Flex gap="3" align="center" my={"2"}>
+          <IssueStatusBadge status={issue.status} />
+          <Text weight={"bold"} size="2">
+            {issue.createdAt.toDateString()}
+          </Text>
+        </Flex>
+        <Card className="prose mt-4 dark:prose-invert">
+          <ReactMarkdown>{issue.description}</ReactMarkdown>
+        </Card>
+      </Box>
+      <Box>
+        <Button>
+          <Pencil2Icon />
+          <Link href={`/issues/${issue.id}/edit`}>Edit Issue</Link>
+        </Button>
+      </Box>
+    </Grid>
   );
 }
